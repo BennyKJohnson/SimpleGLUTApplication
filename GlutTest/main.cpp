627a8fd694a4f20b982d350b7156729ce1674c7a
Benjamin Johnson <bendude@live.com.au>
2016-03-13 11:12:54 +1100

    Initial Commit

---
+++ GlutTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GlutTest
+//
+//  Created by Benjamin Johnson on 13/03/2016.
+//  Copyright © 2016 Benjamin Johnson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
